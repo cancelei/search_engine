b4a0cee2ca226150520cace88b2758059b28aa1f
cancelei <glauber.bannwart@gmail.com>
2024-06-20 18:57:46 -0300

    add google search feature and dashboard update

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,22 +1,36 @@
 # app/controllers/search_controller.rb
+require_relative '../services/google_search_service'
+
 class SearchController < ApplicationController
   def index
     if params[:query].present?
-      bing_service = BingSearchService.new
-      @results = bing_service.search(
-        query: params[:query],
-        count: params[:count],
-        mkt: params[:mkt],
-        safesearch: params[:safesearch],
-        freshness: params[:freshness],
-        sortby: params[:sortby]
-      )
-
-      if user_signed_in?
-        current_user.search_histories.create(query: params[:query])
+      if params[:search_engine] == 'google'
+        google_service = GoogleSearchService.new
+        @results = google_service.search(
+          params[:query],
+          num: params[:count].presence || 10,
+          safesearch: params[:safesearch].presence || 'off'
+        )
+      else
+        bing_service = BingSearchService.new
+        @results = bing_service.search(
+          query: params[:query],
+          count: params[:count].presence || 10,
+          mkt: params[:mkt],
+          safesearch: params[:safesearch],
+          freshness: params[:freshness],
+          sortby: params[:sortby]
+        )
       end
 
-      Rails.logger.debug "Bing API Response: #{@results.inspect}"
+      if user_signed_in?
+        current_user.search_histories.create(
+          query: params[:query],
+          search_engine: params[:search_engine]
+        )
+      end
+
+      Rails.logger.debug "Search API Response: #{@results.inspect}"
     end
   end
 end
--- app/models/search_history.rb
+++ app/models/search_history.rb
@@ -1,4 +1,6 @@
 # app/models/search_history.rb
 class SearchHistory < ApplicationRecord
   belongs_to :user
+  validates :query, presence: true
+  validates :search_engine, presence: true
 end
