a5ad6aa47a80938cce192fad09a7b8758ae3f724
cancelei <glauber.bannwart@gmail.com>
2024-06-24 22:03:58 -0300

    chore: Update JavaScript dependencies and logging configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,6 +65,13 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Log level
+  config.log_level = :debug
+
+  # Ensure logs are output to the console
+  config.logger = ActiveSupport::Logger.new(STDOUT)
+
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
