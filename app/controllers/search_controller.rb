61a880ba6fd48ce682d978778885be4a160e63d3
cancelei <glauber.bannwart@gmail.com>
2024-06-27 19:23:51 -0300

    Add Brave as Search Results Provider.
    chore: Update Readme & environment variables template for API keys

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,20 @@
 # app/controllers/search_controller.rb
 require_relative '../services/google_search_service'
+require_relative '../services/bing_search_service'
+require_relative '../services/brave_search_service'
 
 class SearchController < ApplicationController
   def index
     if params[:query].present?
-      if params[:search_engine] == 'google'
+      case params[:search_engine]
+      when 'google'
         google_service = GoogleSearchService.new
         @results = google_service.search(
           params[:query],
           num: params[:count].presence || 10,
           safesearch: params[:safesearch].presence || 'off'
         )
-      else
+      when 'bing'
         bing_service = BingSearchService.new
         @results = bing_service.search(
           query: params[:query],
@@ -20,6 +23,17 @@ class SearchController < ApplicationController
           safesearch: params[:safesearch],
           freshness: params[:freshness],
           sortby: params[:sortby]
+        )
+      when 'brave'
+        brave_service = BraveSearchService.new
+        @results = brave_service.search(
+          params[:query],
+          country: 'us',
+          search_lang: 'en',
+          ui_lang: 'en-US',
+          count: params[:count].presence || 20,
+          offset: params[:offset].presence || 0,
+          safesearch: params[:safesearch].presence || 'moderate'
         )
       end
 
