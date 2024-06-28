c9766ff2a99e0af0d3bd48e912f008023880c2d5
MSA <crystalbluetech@gmail.com>
2024-06-28 11:25:27 +0100

    expose api using rack-cors to allow react and others connect

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+User.create([
+    { email: 'jane.smith@example.com', encrypted_password: BCrypt::Password.create('password123'), created_at: Time.now, updated_at: Time.now },
+    { email: 'alice.johnson@example.com', encrypted_password: BCrypt::Password.create('password123'), created_at: Time.now, updated_at: Time.now },
+    { email: 'bob.brown@example.com', encrypted_password: BCrypt::Password.create('password123'), created_at: Time.now, updated_at: Time.now },
+    { email: 'charlie.davis@example.com', encrypted_password: BCrypt::Password.create('password123'), created_at: Time.now, updated_at: Time.now }
+  ])
+  
+  User.find_by(email: 'jane.smith@example.com').search_histories.create([
+    { query: 'Best Ruby on Rails tutorials', created_at: Time.now, updated_at: Time.now, search_engine: 'Google' }
+  ])
+  
+  User.find_by(email: 'alice.johnson@example.com').search_histories.create([
+    { query: 'What is the latest Rails version?', created_at: Time.now, updated_at: Time.now, search_engine: 'DuckDuckGo' }
+  ])
+  
+  User.find_by(email: 'bob.brown@example.com').search_histories.create([
+    { query: 'Rails vs Django comparison', created_at: Time.now, updated_at: Time.now, search_engine: 'Bing' }
+  ])
+  
+  User.find_by(email: 'charlie.davis@example.com').search_histories.create([
+    { query: 'Setting up Rails with PostgreSQL', created_at: Time.now, updated_at: Time.now, search_engine: 'Google' }
+  ])
+  
