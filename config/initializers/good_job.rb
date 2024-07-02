3a93706e76fe64832d663bd29e0d27dcfb924b77
cancelei <glauber.bannwart@gmail.com>
2024-07-02 19:14:45 -0300

    Enhance Background Job and monitoring

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,4 +1,5 @@
 GoodJob.configuration do |config|
   config.execution_mode = :async
   config.poll_interval = 30
+  config.preserve_job_records = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount GoodJob::Engine => 'good_job'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
